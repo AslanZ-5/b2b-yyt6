1eff9b2a230a7aa579113f59b15aacd1a2846549
IvanOkoyanniy <okoyanniy36@gmail.com>
2022-10-26 12:07:38 +0300

    Fix user fullname style

--- src/pages/main/numbers/admin/components/info/components/info-blocks/styles.ts
+++ src/pages/main/numbers/admin/components/info/components/info-blocks/styles.ts
@@ -1,47 +1,46 @@
-import { makeStyles, Theme } from "@material-ui/core/styles";
-import { baseColors } from "constants/colors";
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import { baseColors } from 'constants/colors';
 
 export const useStyles = makeStyles((theme: Theme) => ({
   infoBlockContainer: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
     borderRadius: 8,
-    boxShadow: "0 2px 12px 0 rgba(0, 0, 0, 0.08)",
+    boxShadow: '0 2px 12px 0 rgba(0, 0, 0, 0.08)',
     backgroundColor: baseColors.primaryWhite,
-    padding: "20px 24px",
+    padding: '20px 24px',
     marginBottom: 20,
   },
   infoBlockLabel: {
-    minWidth: "45%",
+    minWidth: '45%',
     paddingRight: 10,
-    fontFamily: "PTSans-Bold",
+    fontFamily: 'PTSans-Bold',
     fontSize: 16,
-
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    whiteSpace: "nowrap",
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
   },
   infoBlockValue: {
-    fontFamily: "PTSans-Regular",
+    maxWidth: '180px',
+    fontFamily: 'PTSans-Regular',
     fontSize: 16,
-
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    whiteSpace: "nowrap",
+    wordBreak: 'break-word',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'pre-wrap',
   },
   infoBlockStatusValue: {
-    fontFamily: "PTSans-Regular",
+    fontFamily: 'PTSans-Regular',
     fontSize: 16,
     color: baseColors.primaryBlue,
-
-    overflow: "hidden",
-    textOverflow: "ellipsis",
-    whiteSpace: "nowrap",
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
   },
   infoBlockRow: {
-    display: "flex",
-    alignItems: "center",
+    display: 'flex',
+    alignItems: 'center',
     marginBottom: 8,
   },
 }));
